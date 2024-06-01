8e60738271645fda22813c9a93b61fbc47d4cdf9
Ying Ci <yingci5609@gmail.com>
2024-06-02 01:27:32 +0800

    UI python application

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("Hello world!")
+import tkinter as tk    
+from tkinter import ttk    
+win = tk.Tk()    
+win.title("Python GUI App")    
+ttk.Label(win, text="Hello World").pack()    
+win.mainloop()  
